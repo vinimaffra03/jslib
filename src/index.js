5f788f78b5a7888bae12e81ee4e43651b81dda8d
vinimaffra03 <viniciushmdem15@gmail.com>
2025-04-04 16:35:46 -0700

    separa paragrafos

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
+    quebraEmParagrafos(texto)
     verificaPalavrasDuplicadas(texto)
 })
 
-//console.log(link);
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) => {
+      return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem);
+  }
 
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' ');
@@ -16,5 +23,5 @@ function verificaPalavrasDuplicadas(texto) {
     listaPalavras.forEach(palavra => {
       resultado[palavra] = (resultado[palavra] || 0) + 1
     })
-    console.log(resultado);
+    return resultado
   }
