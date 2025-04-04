15236ae1e3df704883a3a3526cc17ce570c19000
vinimaffra03 <viniciushmdem15@gmail.com>
2025-04-04 16:26:15 -0700

    capturando as palavras

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    console.log(texto);
-    
+    verificaPalavrasDuplicadas(texto)
 })
 
-console.log(link);
+//console.log(link);
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+      resultado[palavra] = (resultado[palavra] || 0) + 1
+    })
+    console.log(resultado);
+  }
